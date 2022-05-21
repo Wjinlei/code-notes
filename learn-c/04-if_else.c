8381d9ca628fa70a7d639fe81fd43fc6a8096413
Jerry Wang <freebufer.wang@gmail.com>
2022-05-21 14:15:39 +0800

    if/else if/else.

---
+++ learn-c/04-if_else.c
@@ -0,0 +1,16 @@
+/* Copyright (c) 2022 Jerry. All Rights Reserved. */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+        int x = 2;
+        if (x > 0) {
+                printf("x is positive.\n");
+        } else if (x < 0) {
+                printf("x is negative.\n");
+        } else {
+                printf("x is zero.\n");
+        }
+        return 0;
+}
