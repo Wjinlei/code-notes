ae7e9e0a98a7ccb306c6d2c5e35d9a661f9ccb5d
Jerry Wang <freebufer.wang@gmail.com>
2022-05-21 14:23:28 +0800

    Logical AND/Logical OR.

---
+++ learn-c/05-and_or.c
@@ -0,0 +1,15 @@
+/* Copyright (c) 2022 Jerry. All Rights Reserved. */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+        int x = 2;
+        if (x > 0 && x < 3) {
+                printf("x > 0 and x < 3.\n");
+        }
+        if (x == 2 || x < 0) {
+                printf("x == 2 or x < 0.\n");
+        }
+        return 0;
+}
