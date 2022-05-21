1af100f85a8753ed693fc09a1519c4a70f147ec0
Jerry Wang <freebufer.wang@gmail.com>
2022-05-21 12:18:20 +0800

    Call function.

---
+++ learn-c/02-call_function.c
@@ -0,0 +1,11 @@
+/* Copyright (c) 2022 Jerry. All Rights Reserved. */
+
+#include <math.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+        double pi = 3.14;
+        printf("sin(pi/2)=%f\n", sin(pi / 2));
+        return 0;
+}
