f69d2d8b3673a26b951b897c4f1382941cb64cd0
Jerry Wang <freebufer.wang@gmail.com>
2022-05-21 12:26:43 +0800

    Modify '02-call_function.c' comment.

--- learn-c/02-call_function.c
+++ learn-c/02-call_function.c
@@ -1,5 +1,10 @@
 /* Copyright (c) 2022 Jerry. All Rights Reserved. */
 
+/*
+ * 使用math.h中声明的库函数还有一点特殊之处，gcc命令行必须加-lm选项，
+ * 因为数学函数位于libm.so库文件中（这些库文件通常位于/lib目录下），
+ * -lm选项告诉编译器，我们程序中用到的数学函数要到这个库文件里找。
+ */
 #include <math.h>
 #include <stdio.h>
 
