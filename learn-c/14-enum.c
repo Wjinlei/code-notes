b1558a1d2bcec1d36cfba41e54819603601c71b2
Jerry Wang <freebufer.wang@gmail.com>
2022-05-23 15:25:56 +0800

    enum.

---
+++ learn-c/14-enum.c
@@ -0,0 +1,39 @@
+/* Copyright (c) 2022 Jerry. All Rights Reserved. */
+
+#include <stdio.h>
+
+/* 枚举类型
+ * 枚举类型的成员是常量,它们的值由编译器自动分配
+ * 枚举成语的值由0开始
+ * 枚举常量也是一种整型，其值在编译时确定，因此也可以出现在常量表达式中，
+ * 可以用于初始化全局变量或者作为case分支的判断条件。
+ */
+enum enum1 {
+        AAA,
+        BBB
+};
+
+/*
+ * 如果不希望从0开始分配，可以这样定义：
+ */
+enum enum2 {
+        /*
+         * 有一点需要注意，虽然结构体的成员名和变量名不在同一命名空间中，
+         * 但枚举的成员名却和变量名在同一命名空间中，
+         * 所以会出现命名冲突。例如下面再定义AAA和BBB是不合法的：
+         */
+        // AAA = 1,
+        // BBB,
+        CCC = 1,
+        DDD
+};
+
+int main(int argc, char* argv[])
+{
+
+        printf("%d\n", AAA);
+        printf("%d\n", BBB);
+        printf("%d\n", CCC);
+        printf("%d\n", DDD);
+        return 0;
+}
