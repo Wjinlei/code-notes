0b8d8afe9ed6bbfcf09c4c4a18ed4d4e26882e39
Jerry Wang <freebufer.wang@gmail.com>
2022-05-23 16:50:19 +0800

    Update array.

--- learn-c/15-array.c
+++ learn-c/15-array.c
@@ -7,18 +7,24 @@
 int main(int argc, char* argv[])
 {
         /* 定义长度为4的int型数组,并为其赋初值,未赋初值的元素也是用0来初始化 */
-	int count1[4] = { 3, 2, };
+        int count1[4] = {
+                3,
+                2,
+        };
         int i;
-	for (i = 0; i < 4; i++)
-        {
-		printf("count1[%d]=%d\n", i, count1[i]);
+        for (i = 0; i < 4; i++) {
+                printf("count1[%d]=%d\n", i, count1[i]);
         }
 
-        /* 
+        /*
          * 如果定义数组的同时初始化它，也可以不指定数组的长度,
          * 编译器会根据Initializer有三个元素确定数组的长度为3。
          */
-        int count2[] = { 3, 2, 1, };
+        int count2[] = {
+                3,
+                2,
+                1,
+        };
 
         /* 数组不能相互赋值或初始化。例如这样是错的： */
         // int a[5] = { 4, 3, 2, 1 };
