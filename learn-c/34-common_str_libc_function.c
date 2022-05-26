47af2dab5b331693dfd8f4c515d4fde5f0d03ba7
Jerry Wang <freebufer.wang@gmail.com>
2022-05-26 16:12:47 +0800

    Common string function.

---
+++ learn-c/34-common_str_libc_function.c
@@ -0,0 +1,37 @@
+/* 字符串操作函数 */
+
+/*
+ *
+ * 初始化字符串
+ * void *memset(void *s, int c, size_t n);
+ *
+ * 取字符串长度
+ * size_t strlen(const char *s);
+ *
+ * 拷贝字符串
+ * char *strcpy(char *dest, const char *src);
+ * char *strncpy(char *dest, const char *src, size_t n);
+ * void *memcpy(void *dest, const void *src, size_t n);
+ * void *memmove(void *dest, const void *src, size_t n);
+ *
+ * 连接字符串
+ * char *strcat(char *dest, const char *src);
+ * char *strncat(char *dest, const char *src, size_t n);
+ *
+ * 比较字符串
+ * int strcmp(const char *s1, const char *s2);
+ * int strncmp(const char *s1, const char *s2, size_t n);
+ * int memcmp(const void *s1, const void *s2, size_t n);
+ * int strcasecmp(const char *s1, const char *s2);
+ * int strncasecmp(const char *s1, const char *s2, size_t n);
+ *
+ * 搜索字符串
+ * char *strchr(const char *s, int c);
+ * char *strrchr(const char *s, int c);
+ * char *strstr(const char *haystack, const char *needle);
+ *
+ * 分割字符串
+ * char *strtok(char *str, const char *delim);
+ * char *strtok_r(char *str, const char *delim, char **saveptr);
+ *
+ */
