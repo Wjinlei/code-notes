ed475f60f62952661d8701e9080c7778b6ab4fa9
Jerry Wang <freebufer.wang@gmail.com>
2022-05-28 01:07:52 +0800

    Update hook_openat.

--- learn-c/kernel_module/02-hook_openat/hook_openat.c
+++ learn-c/kernel_module/02-hook_openat/hook_openat.c
@@ -2,17 +2,39 @@
 
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jerry Wang <freebufer.wang@gmail.com>");
 MODULE_DESCRIPTION("Hello World");
 MODULE_VERSION("0.1");
 
-// long sys_kill(const struct pt_regs *regs) -- modern kernels
+static char* duplicate_filename(const char __user* filename)
+{
+        char* kernel_filename;
+        kernel_filename = kmalloc(4096, GFP_KERNEL);
+        if (!kernel_filename) {
+                return NULL;
+        }
+        if (strncpy_from_user(kernel_filename, filename, 4096) < 0) {
+                kfree(kernel_filename);
+                return NULL;
+        }
+        return kernel_filename;
+}
+
+// hook openat reject create testfile
 KHOOK_EXT(long, __x64_sys_openat, const struct pt_regs*);
 static long khook___x64_sys_openat(const struct pt_regs* regs)
 {
-        pr_info("Hello World\n");
+        char* kernel_filename;
+        kernel_filename = duplicate_filename((char*)regs->si);
+        if (strcmp(kernel_filename, "testfile") == 0) {
+                pr_info("hook: %d %s\n", current->pid, kernel_filename);
+                kfree(kernel_filename);
+                return 0;
+        }
+        kfree(kernel_filename);
         return KHOOK_ORIGIN(__x64_sys_openat, regs);
 }
 
