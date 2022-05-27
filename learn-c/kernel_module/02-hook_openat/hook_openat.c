b8e99f8065d80de4a79048f83d9e85bcc24f69d6
Jerry Wang <freebufer.wang@gmail.com>
2022-05-28 01:58:10 +0800

    Update hook_openat.

--- learn-c/kernel_module/02-hook_openat/hook_openat.c
+++ learn-c/kernel_module/02-hook_openat/hook_openat.c
@@ -1,40 +1,25 @@
-#include "khook/engine.c"
-
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/slab.h>
 
+#include "khook/engine.c"
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jerry Wang <freebufer.wang@gmail.com>");
-MODULE_DESCRIPTION("Hello World");
+MODULE_DESCRIPTION("Hook openat demo");
 MODULE_VERSION("0.1");
 
-static char* duplicate_filename(const char __user* filename)
-{
-        char* kernel_filename;
-        kernel_filename = kmalloc(4096, GFP_KERNEL);
-        if (!kernel_filename) {
-                return NULL;
-        }
-        if (strncpy_from_user(kernel_filename, filename, 4096) < 0) {
-                kfree(kernel_filename);
-                return NULL;
-        }
-        return kernel_filename;
-}
-
-// hook openat reject create testfile
+/* hook openat reject create testfile */
 KHOOK_EXT(long, __x64_sys_openat, const struct pt_regs*);
 static long khook___x64_sys_openat(const struct pt_regs* regs)
 {
-        char* kernel_filename;
-        kernel_filename = duplicate_filename((char*)regs->si);
-        if (strcmp(kernel_filename, "testfile") == 0) {
-                pr_info("hook: %d %s\n", current->pid, kernel_filename);
-                kfree(kernel_filename);
-                return 0;
+        char kernel_filename[2048];
+        if (strncpy_from_user(kernel_filename, (char*)regs->si, 2048) > 0) {
+                if (strcmp(kernel_filename, "testfile") == 0) {
+                        pr_info("Hook openat: [%d] %s -> %s\n", current->pid, current->comm, kernel_filename);
+                        return -1;
+                }
         }
-        kfree(kernel_filename);
         return KHOOK_ORIGIN(__x64_sys_openat, regs);
 }
 
